bfa6f9075267a546077a40577d41e00b9a0beedf
LuizGyro <luizfelipe.g@hotmail.com>
2019-10-03 16:31:15 -0300

    Laid down basic process structure for IMMORTAL computer.

--- ep2-VictorAraujo-LuizGirotto/ep02.c
+++ ep2-VictorAraujo-LuizGirotto/ep02.c
@@ -22,6 +22,7 @@
 #include <stdbool.h>
 
 #include "file.h"
+#include "ll.h"
 
 
 /* ///////////////////////////////////////////// */
@@ -39,6 +40,8 @@ bool is_working = false; /* Only for workers */
 bool want_election = false; /* Only for workers */
 
 int *work_array; /* Only for immortal. 0 means that the file still needs to be ordered. */
+celula *alive_list; /* Only for immortal. Is sent on end of heartbeat to Lider */
+celula *working_list; /* Only for immortal. Is sent on end of heartbeat to Lider */
 
 /* General control variables */
 bool all_work_done = false; /* Specifies when all work is done, and process should be terminated */
@@ -53,6 +56,7 @@ bool all_work_done = false; /* Specifies when all work is done, and process shou
 /* ///////////////////////////////////////////// */
 
 int main( int argc, char **argv) {
+    int i;
     pid_t childpid;
 
     if (argc > 1) {
@@ -63,18 +67,63 @@ int main( int argc, char **argv) {
         /* begin working (IMMORTAL-Worker) while waiting for     */
         /* other computers to join in. */
         int n_files = splitFiles(argv[1]);
-
-
+        n_files++;
+        work_array = malloc (n_files * sizeof(int));
+        for (i = 0; i < n_files; i++) {
+            work_array[i] = 0;
+        }
 
         if ((childpid = fork()) == 0) {
-            /* Is LEADER-Generic */
-            is_leader = true;
+            /* Is IMMORTAL-Listener */
             exit(0);
         }
         else {
-            /* Is IMMORTAL-Generic */
+            if ((childpid = fork()) == 0) {
+                /* Is WORKER-Generic (but also IMMORTAL) */
+                /* Code here should match beggining of regular worker code */
+                /* (Except when starting as is_leader) */
 
-            exit(0);
+                is_worker = true;
+                if ((childpid = fork()) == 0) {
+                    /* Is WORKER-Listener (but also IMMORTAL) */
+                    exit(0);
+                }
+                else {
+                    /* Is WORKER-Generic (but also IMMORTAL) */
+                    /* No final do loop WORKER-Generic, se temos is_leader, */
+                    /* mas estamos no processo do WORKER-Generic, criamos */
+                    /* um novo processo LEADER-Generic, e tiramos o */
+                    /* is_leader do WORKER-Generic */
+
+                    is_leader = true;
+
+                    while(!all_work_done) {
+                        if (is_leader) {
+                            if ((childpid = fork()) == 0) {
+                                /* Is LEADER-Generic (but also IMMORTAL) */
+                                is_worker = false;
+                                if ((childpid = fork()) == 0) {
+                                    /* Is LEADER-Listener (but also IMMORTAL) */
+                                    exit(0);
+                                }
+                                else {
+                                    /* Is LEADER-Generic (but also IMMORTAL) */
+                                    /* LEADER-Generic loop goes here */
+                                    exit(0);
+                                }
+                            }
+                            is_leader = false;
+                        }
+                    }
+
+                    exit(0);
+
+                }
+            }
+            else {
+                /* Is IMMORTAL-Generic */
+                exit(0);
+            }
         }
 
 
@@ -82,7 +131,7 @@ int main( int argc, char **argv) {
 
     }
     else {
-        /* Is, for now, WORKER-Listener */
+        /* Is WORKER-Generic */
         is_worker = true;
     }
 
