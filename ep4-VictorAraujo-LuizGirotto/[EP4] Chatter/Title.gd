2f964c1f0e16988263422221f679bc7ef82ba0d4
LuizGyro <luizfelipe.g@hotmail.com>
2019-11-25 22:56:53 -0300

    Rename irc_chats

--- ep4-VictorAraujo-LuizGirotto/[EP4] Chatter/Title.gd
+++ ep4-VictorAraujo-LuizGirotto/[EP4] Chatter/Title.gd
@@ -1,1 +1,16 @@
 extends Control
+
+
+func _on_Button_pressed():
+#	var a = PoolByteArray([20, 0, 0, 0, 0, 0, 0, 255])
+#	var m = var2bytes(a)
+#	for n in m:
+#		print(n)
+#	bytes2var(m)
+
+#	var message = PoolByteArray([20, 0, 0, 0, 0, 0, 0, 255])
+#	$TextEdit.insert_text_at_cursor(message.get_string_from_ascii())
+#	print(message.get_string_from_ascii())
+	bytes2var(PoolByteArray([20, 0, 0, 0, 0, 0, 0, 255]))
+
+#	bytes2var([20, 0, 0, 0, 0, 0, 0, 255])
